3037d4c10d79ce101caf7b4ed2a837c80744c886
ChetanSubhash3520 <chunduruchetansubhash@gmail.com>
2025-03-27 20:35:02 -0400

    final commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/Csprofile.jpeg";
+import profilePic from "../assets/about.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
