58d5086958c7620e469339e192099fb387765108
ChetanSubhash3520 <chunduruchetansubhash@gmail.com>
2025-03-27 21:47:57 -0400

    updated final commit

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,12 +1,8 @@
-import { RiReactjsLine } from "react-icons/ri"
-import { TbBrandTypescript } from "react-icons/tb"
-import { TbBrandTailwind } from "react-icons/tb"
-import { TbBrandRedux } from "react-icons/tb"
-import { TbBrandGraphql } from "react-icons/tb"
-import { FaNodeJs } from "react-icons/fa"
-import { BiLogoAws } from "react-icons/bi"
-import { SiRubyonrails } from "react-icons/si";
-import { SiMysql } from "react-icons/si";
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandTypescript, TbBrandTailwind, TbBrandRedux, TbBrandGraphql } from "react-icons/tb";
+import { FaNodeJs, FaMicroscope } from "react-icons/fa";
+import { BiLogoAws, BiCodeAlt } from "react-icons/bi";
+import { SiMysql, SiSpring, SiDocker, SiTerraform, SiJunit5 } from "react-icons/si";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -21,10 +17,11 @@ const iconVariants = (duration) => ({
     },
   },
 });
+
 function Technologies() {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <motion.h2
+      <motion.h2
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 1.5 }}
@@ -38,81 +35,114 @@ function Technologies() {
         transition={{ duration: 1.5 }}
         className="flex flex-wrap items-center justify-center gap-9"
       >
+        {/* Current technologies */}
         <motion.div
           variants={iconVariants(2.5)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-                <RiReactjsLine className="text-7xl text-cyan-400"/>
-            </motion.div>
-            <motion.div
+          <RiReactjsLine className="text-7xl text-cyan-400"/>
+        </motion.div>
+        <motion.div
           variants={iconVariants(3)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-                <TbBrandRedux className="text-7xl text-purple-400"/>
-            </motion.div>
-            <motion.div
+          <TbBrandRedux className="text-7xl text-purple-400"/>
+        </motion.div>
+        <motion.div
           variants={iconVariants(6)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-                <TbBrandTypescript className="text-7xl text-blue-400"/>
-            </motion.div>
-            <motion.div
-          variants={iconVariants(4)}
-          initial="initial"
-          animate="animate"
-          className="rounded-2xl border-4 border-neutral-800 p-4"
-        >
-                <SiRubyonrails className="text-7xl text-red-400"/>
-            </motion.div>
-            <motion.div
+          <TbBrandTypescript className="text-7xl text-blue-400"/>
+        </motion.div>
+        <motion.div
           variants={iconVariants(8)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-                <TbBrandTailwind className="text-7xl text-cyan-400"/>
-            </motion.div>
-            <motion.div
+          <TbBrandTailwind className="text-7xl text-cyan-400"/>
+        </motion.div>
+        <motion.div
           variants={iconVariants(2)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-                <TbBrandGraphql className="text-7xl text-pink-400"/>
-            </motion.div>
-            <motion.div
+          <TbBrandGraphql className="text-7xl text-pink-400"/>
+        </motion.div>
+        <motion.div
           variants={iconVariants(3.5)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-                <FaNodeJs className="text-7xl text-green-400"/>
-            </motion.div>
-            <motion.div
+          <FaNodeJs className="text-7xl text-green-400"/>
+        </motion.div>
+        <motion.div
           variants={iconVariants(1)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-                <BiLogoAws className="text-7xl text-orange-400"/>
-            </motion.div>
-            <motion.div
+          <BiLogoAws className="text-7xl text-orange-400"/>
+        </motion.div>
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
-                <SiMysql className="text-7xl text-grey-400"/>
-            </motion.div>   
+          <SiMysql className="text-7xl text-gray-400"/>
         </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiSpring className="text-7xl text-green-500"/>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiDocker className="text-7xl text-blue-500"/>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.8)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiJunit5 className="text-7xl text-red-500"/>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaMicroscope className="text-7xl text-purple-500"/>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.7)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiTerraform className="text-7xl text-purple-600"/>
+        </motion.div>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default Technologies
+export default Technologies;
